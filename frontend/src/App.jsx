762d425f3845e4b7c624c978c3a684526d3a26b4
Karthick Chandran <karthick_c83@hotmail.com>
2026-02-04 20:48:22 +1300

    Update HashRouter to BrowserRouter  for AWS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { HashRouter  as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter  as Router, Routes, Route, Link } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
